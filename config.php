5306202385f02d7abbae69546faefc2a546d1088
RyotaroSeto <ryotaroseto@RyotaroSetonoMacBook-Pro.local>
2021-01-08 23:44:34 +0900

    add TODO_APP

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN', 'mysql:host=localhost;dbname=TODO');
+define('DB_USERNAME', 'suzuki');
+define('DB_PASSWORD', 'Ryotaro03');
---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>menu</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div id="container">
+        <td>Todos</td></br>
+        <a href= "index.php" >Todo機能へ</a></br>
+        <a href= "calender/index.php" >カレンダー機能へ</a></br>
+        <a href= "menu.php" >メニューへ</a></br>
+        <a href= "menu.php" >メニューへ</a>
+    </div>
+</body>
+</html>
